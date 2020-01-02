0bf7ea7ade7b9ad2f31e23a518ba90a011c6d458
Chao Pang <ChaoPang229@gmail.com>
2020-01-01 20:08:23 -0500

    changed concept_id to standard_concept_id in the embedding dataframe

--- spark/tools/extract_glove_embeddings.py
+++ spark/tools/extract_glove_embeddings.py
@@ -10,9 +10,9 @@ def main(spark, glove_model_path, output_folder):
     glove = Glove.load(glove_model_path)
 
     dictionary_schema = T.StructType([T.StructField('index', T.IntegerType(), True),
-                                      T.StructField('concept_id', T.IntegerType(), True)])
+                                      T.StructField('standard_concept_id', T.IntegerType(), True)])
 
-    dictionary_df = spark.createDataFrame([Row(index=k, concept_id=int(v)) for k, v in glove.inverse_dictionary.items()],
+    dictionary_df = spark.createDataFrame([Row(index=k, standard_concept_id=int(v)) for k, v in glove.inverse_dictionary.items()],
                                           dictionary_schema)
 
     vector_schema = T.StructType([T.StructField('index', T.IntegerType(), True),
@@ -21,7 +21,7 @@ def main(spark, glove_model_path, output_folder):
     vector_df = spark.createDataFrame([Row(index=idx, vector=vector.tolist()) for idx, vector in enumerate(glove.word_vectors)],
                                   vector_schema)
 
-    dictionary_df.join(vector_df, 'index').select('concept_id', 'vector').write.mode('overwrite').parquet(output_folder)
+    dictionary_df.join(vector_df, 'index').select('standard_concept_id', 'vector').write.mode('overwrite').parquet(output_folder)
 
 if __name__ == '__main__':
 
