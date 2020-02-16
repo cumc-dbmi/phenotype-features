62d82c3ec657f94cc2955609d255e71ff9bddf10
Chao Pang <chaopang229@gmail.com>
2020-02-15 20:20:09 -0500

    added join_domain_tables using visit_occurrence

--- spark/tools/generate_glove_training_data.py
+++ spark/tools/generate_glove_training_data.py
@@ -1,3 +1,4 @@
+import logging
 import argparse
 import pyspark.sql.functions as F
 import pyspark.sql.types as T
@@ -33,6 +34,45 @@ def join_domain_tables(domain_tables):
 
     return patient_event
 
+def join_domain_tables_to_visit(domain_tables, visit_occurrence):
+    
+    """Join domain tables to visit_occurrence
+    
+    Keyword arguments:
+    domain_tables -- the array containing the OMOOP domain tabls except visit_occurrence
+    visit_occurrence -- the OMOP visit_occurrence table
+    
+    The function is to join each domain table to visit_occurrence to limit the records that have a valid visit_occurrence_id.
+    In addition, the output columns of the domain table is converted to the same standard format as the following 
+    (person_id, time_window, standard_concept_id, domain). In this case, co-occurrence is defined as those concept ids that have co-occurred within the same visit. 
+    
+    """
+    
+    patient_event = None
+    
+    for domain_table in domain_tables:
+        #extract the domain concept_id from the table fields. E.g. condition_concept_id from condition_occurrence
+        concept_id_field = [f.name for f in domain_table.schema.fields if "concept_id" in f.name][0]
+        #extract the name of the table
+        table_domain_field = concept_id_field.replace("_concept_id", "")
+        #limit the domain records to those which have a visit_occurrence_id
+        joined_domain_table = domain_table \
+            .join(visit_occurrence, domain_table["visit_occurrence_id"] == visit_occurrence["visit_occurrence_id"])
+        #standardize the output columns
+        domain_table = joined_domain_table \
+            .select(domain_table["person_id"], 
+                domain_table["visit_occurrence_id"].alias("partition"), 
+                domain_table[concept_id_field].alias("standard_concept_id"), 
+                F.lit(table_domain_field).alias("domain"))
+
+        if patient_event == None:
+            patient_event = domain_table
+        else:
+            patient_event = patient_event.union(domain_table)
+        
+    return patient_event
+
+
 def roll_up_to_drug_ingredients(patient_event, concept, concept_ancestor):
 
     drug_ingredient = patient_event.where('domain="drug"').select('standard_concept_id').distinct() \
@@ -40,14 +80,15 @@ def roll_up_to_drug_ingredients(patient_event, concept, concept_ancestor):
         .join(concept, F.col('ancestor_concept_id') == F.col('concept_id')) \
         .where(concept['concept_class_id'] == 'Ingredient') \
         .select(F.col('standard_concept_id').alias('drug_concept_id'), F.col('concept_id').alias('ingredient_concept_id'))
-
+    
+    patient_event_fields = [F.col(field_name) for field_name in patient_event.schema.fieldNames() if field_name != 'standard_concept_id']
+    patient_event_fields.extend([F.coalesce(F.col('ingredient_concept_id'), F.col('standard_concept_id')).alias('standard_concept_id')])
+    
     patient_event = patient_event.join(drug_ingredient,
            (F.col('standard_concept_id') == F.col('drug_concept_id')) &
            (F.col('domain') == 'drug'), 'left_outer') \
-        .select(F.col('person_id'),
-                F.coalesce(F.col('ingredient_concept_id'), F.col('standard_concept_id')).alias('standard_concept_id'),
-                F.col('date'),
-                F.col('domain'))
+        .select(patient_event_fields)
+    
     return patient_event
 
 def creat_partitions(patient_event, start_date, end_date, window_size):
@@ -83,7 +124,7 @@ def create_file_path(input_folder, table_name):
     return file_path
 
 if __name__ == "__main__":
-    
+
     parser = argparse.ArgumentParser(description='Aruguments for generating patient sequences from OMOP tables')
     
     parser.add_argument('-i',
@@ -107,19 +148,28 @@ if __name__ == "__main__":
                         help='The list of omop tables',
                         required=True)
     
+    parser.add_argument('-v',
+                        '--visit_occurrence',
+                        dest='visit_occurrence',
+                        action='store_true',
+                        default=False,
+                        required=False)
+    
     parser.add_argument('-s',
                         '--start_year',
                         dest='start_year',
                         action='store',
                         help='The start year for the time window',
-                        required=True)
+                        default=1985,
+                        required=False)
     
     parser.add_argument('-w',
                         '--window_size',
                         dest='window_size',
                         action='store',
                         help='The size of the time window',
-                        required=True)
+                        default=30,
+                        required=False)
 
     
     ARGS = parser.parse_args()
@@ -127,7 +177,12 @@ if __name__ == "__main__":
     spark = SparkSession.builder.appName("Generate patient sequences").getOrCreate()
     spark.sparkContext.setLogLevel("WARN")
     
-   
+    log4jLogger = spark.sparkContext._jvm.org.apache.log4j
+    LOGGER = log4jLogger.LogManager.getLogger(__name__)
+    LOGGER.warn("pyspark script logger initialized")
+    
+    #logger = logging.getLogger('py4j')
+
     domain_tables = []
     for domain_table_name in ARGS.omop_table_list:
         domain_table = spark.read.parquet(create_file_path(ARGS.input_folder, domain_table_name))
@@ -136,11 +191,32 @@ if __name__ == "__main__":
     concept = spark.read.parquet(create_file_path(ARGS.input_folder, 'concept'))
     concept_ancestor = spark.read.parquet(create_file_path(ARGS.input_folder, 'concept_ancestor'))
     
-    start_date = str(datetime.date(int(ARGS.start_year), 1, 1))
-    end_date = datetime.datetime.now()
-    window_size = ARGS.window_size
+    if ARGS.visit_occurrence:
+        
+        LOGGER.warn('Generate event sequences using visit_occurrence for {omop_table_list}' \
+                         .format(omop_table_list=ARGS.omop_table_list))
+        
+        visit_occurrence = spark.read.parquet(create_file_path(ARGS.input_folder, 'visit_occurrence'))
+        patient_event = join_domain_tables_to_visit(domain_tables, visit_occurrence)
+        patient_event = roll_up_to_drug_ingredients(patient_event, concept, concept_ancestor)
+    else:
+        
+        start_date = str(datetime.date(int(ARGS.start_year), 1, 1))
+        end_date = datetime.datetime.now()
+        window_size = ARGS.window_size
+        
+        LOGGER.warn('''Generate event sequences using the time window configuration 
+        start_date={start_date}
+        end_date={end_date}
+        window_size={window_size}
+        omop_table_list={omop_table_list}'''.format(start_date=start_date,
+                                                    end_date=end_date,
+                                                    window_size=window_size,
+                                                    omop_table_list=ARGS.omop_table_list))
+        
+        patient_event = join_domain_tables(domain_tables)
+        patient_event = roll_up_to_drug_ingredients(patient_event, concept, concept_ancestor)
+        patient_event = creat_partitions(patient_event, start_date, end_date, window_size)
+        
 
-    patient_event = join_domain_tables(domain_tables)
-    patient_event = roll_up_to_drug_ingredients(patient_event, concept, concept_ancestor)
-    patient_event = creat_partitions(patient_event, start_date, end_date, window_size)
     generate_sequences(patient_event, output_path=ARGS.output_folder)
