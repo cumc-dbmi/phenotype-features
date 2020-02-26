94854afb1a6d7b770499c8bcdce1017740a0dc70
Chao Pang <chaopang229@gmail.com>
2020-02-26 17:26:46 -0500

    added a sub_window to pre-compute the patient sequences in the mini obsevation periods before concatenating them for the defined time window

--- spark/tools/generate_glove_training_data.py
+++ spark/tools/generate_glove_training_data.py
@@ -8,7 +8,8 @@ import datetime
 from run_glove_on_omop_tables import *
 from extract_glove_embeddings import *
 
-NUM_PARTITIONS = 400
+NUM_PARTITIONS = 800
+SUB_WINOW_THRESHOLD = 30
 
 
 def join_domain_tables(domain_tables):
@@ -62,25 +63,46 @@ def roll_up_to_drug_ingredients(patient_event, concept, concept_ancestor):
     
     return patient_event
 
-def create_time_window_partitions(patient_event, start_date, end_date, window_size):
-    patient_data = patient_event \
+def create_time_window_partitions(patient_event, start_date, end_date, window_size, output_path):
+    
+    # if the window_size is bigger than SUB_WINOW_THRESHOLD, create the sub_windows for scalable computations
+    sub_window_size = SUB_WINOW_THRESHOLD if window_size > SUB_WINOW_THRESHOLD else window_size
+    
+    patient_event = patient_event \
         .withColumnRenamed('date', 'unix_date') \
         .withColumn('date', F.from_unixtime('unix_date').cast(T.DateType())) \
         .withColumn('start_date', F.lit(start_date).cast(T.DateType())) \
         .withColumn('end_date', F.lit(end_date).cast(T.DateType())) \
-        .withColumn('patient_segment', (F.datediff('date', 'start_date') / window_size).cast(T.IntegerType()))
-    return patient_data
+        .withColumn('patient_window', (F.datediff('date', 'start_date') / window_size).cast(T.IntegerType())) \
+        .withColumn('sub_patient_window', (F.datediff('date', 'start_date') / sub_window_size).cast(T.IntegerType()))
+
+    patient_event.repartition(NUM_PARTITIONS, 'person_id', 'sub_patient_window') \
+        .write.mode('overwrite').parquet(output_path)
+
+def create_visit_partitions(patient_event, output_path):
+    
+    patient_event = patient_event \
+        .where(F.col('visit_occurrence_id').isNotNull()) \
+        .withColumnRenamed('visit_occurrence_id', 'patient_window') \
+        .withColumn('sub_patient_window', F.col('patient_window'))
+    
+    patient_event.repartition(NUM_PARTITIONS, 'person_id', 'sub_patient_window') \
+        .write.mode('overwrite').parquet(output_path)
+
 
 def generate_sequences(patient_event, output_path):
 
     join_collection_udf = F.udf(lambda its: ' '.join([str(item) for item in its]), T.StringType())
-
-    patient_event.orderBy('person_id', 'patient_segment', 'date') \
-        .repartition(NUM_PARTITIONS, 'person_id', 'patient_segment') \
-        .select('person_id', 'standard_concept_id', 'patient_segment') \
-        .groupBy('person_id', 'patient_segment') \
-        .agg(join_collection_udf(F.collect_list('standard_concept_id')).alias('concept_list'),
-             F.size(F.collect_list('standard_concept_id')).alias('collection_size')) \
+    
+    patient_event.orderBy('person_id', 'sub_patient_window', 'date') \
+        .repartition(NUM_PARTITIONS, 'person_id', 'sub_patient_window', 'patient_window') \
+        .select('person_id', 'standard_concept_id', 'sub_patient_window', 'patient_window') \
+        .groupBy('person_id', 'sub_patient_window', 'patient_window') \
+        .agg(join_collection_udf(F.collect_list('standard_concept_id')).alias('sub_window_concept_list'),
+             F.size(F.collect_list('standard_concept_id')).alias('sub_window_collection_size')) \
+        .groupBy('person_id', 'patient_window') \
+        .agg(join_collection_udf(F.collect_list('sub_window_concept_list')).alias('concept_list'),
+             F.sum('sub_window_collection_size').alias('collection_size')) \
         .where(F.col('collection_size') > 1) \
         .write.mode('overwrite').parquet(output_path)
 
@@ -103,17 +125,18 @@ def get_patient_sequence_folder(output_folder):
 def get_patient_sequence_csv_folder(output_folder):
     return create_file_path(output_folder, 'patient_sequence_csv')
 
-def repartition_patient_event(spark, patient_event, output_folder_path):
-    patient_event.repartition(NUM_PARTITIONS, 'person_id', 'patient_segment') \
-        .write.mode('overwrite').parquet(output_folder_path)
-    return spark.read.parquet(output_folder_path)
-
 def write_sequences_to_csv(spark, patient_sequence_path, patient_sequence_csv_path):
     spark.read.parquet(patient_sequence_path).select('concept_list').repartition(1) \
         .write.mode('overwrite').option('header', 'false').csv(patient_sequence_csv_path)
 
 
 # -
+
+def repartition_patient_event(spark, patient_event, output_folder_path):
+    patient_event.repartition(NUM_PARTITIONS, 'person_id', 'patient_segment') \
+        .write.mode('overwrite').parquet(output_folder_path)
+    return spark.read.parquet(output_folder_path)
+
 
 def generate_patient_sequence(spark, 
                               input_folder, 
@@ -139,9 +162,9 @@ def generate_patient_sequence(spark,
     if is_visit_based:
         LOGGER.info('Generating event sequences using visit_occurrence_id for {omop_table_list}' \
                          .format(omop_table_list=omop_table_list))
-
-        patient_event = patient_event.where(F.col('visit_occurrence_id').isNotNull()) \
-            .withColumnRenamed('visit_occurrence_id', 'patient_segment')
+        
+        patient_event = create_visit_partitions(patient_event, 
+                                                get_patient_event_folder(output_folder))
     else:
         start_date = str(datetime.date(int(start_year), 1, 1))
         end_date = datetime.datetime.now()
@@ -155,10 +178,14 @@ def generate_patient_sequence(spark,
                                                         window_size=window_size,
                                                         omop_table_list=omop_table_list))
         
-        patient_event = create_time_window_partitions(patient_event, start_date, end_date, window_size)
+        patient_event = create_time_window_partitions(patient_event, 
+                                                      start_date, 
+                                                      end_date, 
+                                                      window_size, 
+                                                      get_patient_event_folder(output_folder))
     
-    repartitioned_patient_event = repartition_patient_event(spark, patient_event, get_patient_event_folder(output_folder))
-    generate_sequences(repartitioned_patient_event, get_patient_sequence_folder(output_folder))
+    patient_event = spark.read.parquet(get_patient_event_folder(output_folder))
+    generate_sequences(patient_event, get_patient_sequence_folder(output_folder))
     write_sequences_to_csv(spark, get_patient_sequence_folder(output_folder), get_patient_sequence_csv_folder(output_folder))
 
 
@@ -197,6 +224,7 @@ def parse_args():
         
     parser.add_argument('-n',
                         '--no_components',
+                        type=int,
                         dest='no_components',
                         action='store',
                         help='vector size',
@@ -205,6 +233,7 @@ def parse_args():
 
     parser.add_argument('-e',
                         '--epochs',
+                        type=int,
                         dest='epochs',
                         action='store',
                         help='The number of epochs that the algorithm will run',
@@ -213,6 +242,7 @@ def parse_args():
     
     parser.add_argument('-s',
                         '--start_year',
+                        type=int,
                         dest='start_year',
                         action='store',
                         help='The start year for the time window',
@@ -221,6 +251,7 @@ def parse_args():
     
     parser.add_argument('-w',
                         '--window_size',
+                        type=int,
                         dest='window_size',
                         action='store',
                         help='The size of the time window',
